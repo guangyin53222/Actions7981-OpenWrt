56f8a42cb7b606d72532ba22d7c9829dd6d5fa21
guangyin53222 <532225662@qq.com>
2026-07-23 17:30:08 +0800

    Update diy-part1.sh

--- MT7981-eMMC-USB-hanwckf/diy-part1.sh
+++ MT7981-eMMC-USB-hanwckf/diy-part1.sh
@@ -8,11 +8,12 @@
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
-#!/bin/bash
+
 # 加载files自定义配置文件
 if [ -d "$GITHUB_WORKSPACE/files" ]; then
     cp -r $GITHUB_WORKSPACE/files openwrt/
 fi
+
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # Add a feed source
